5e5f4b8ca17f339b7992a44784b6777e0cf543ef
mbsii3 <mshortjr3@gmail.com>
2023-03-19 13:51:09 -0500

    add Item model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=150)
+
+    def __str__(self):
+        return self.name
