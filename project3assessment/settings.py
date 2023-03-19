3ee17495e73f0e99296398d3d90acb6f560c9ab1
mbsii3 <mshortjr3@gmail.com>
2023-03-19 13:15:40 -0500

    setup database and mainapp

--- project3assessment/settings.py
+++ project3assessment/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +81,12 @@ WSGI_APPLICATION = 'project3assessment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mbsii3/project3assessment',
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PW'],
+        'HOST': 'db.bit.io',
+        'PORT': '5432',
     }
 }
 
