71d02e889566c6a461c2c46108e17983fab39d26
mbsii3 <mshortjr3@gmail.com>
2023-03-19 14:12:33 -0500

    add item index template and render items from db

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('items/', views.items_index, name='items_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Item
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def items_index(request):
+    items = Item.objects.all()
+    return render(request, 'items/index.html', { 'items': items })
